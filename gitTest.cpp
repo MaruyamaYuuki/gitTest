c72a50856bde3f6109d59026de7c33fadedf0a55
勇樹 丸山 <k023g0106@g.neec.ac.jp>
2023-10-18 11:49:49 +0900

    Playerを作成

--- gitTest.cpp
+++ gitTest.cpp
@@ -8,7 +8,9 @@ int main()
     std::cout << "Hello GitHub!\n";
     std::cout << "LC1B_25_マルヤマユウキ\n";
 
-    std::cout << "2023/10/11";
+    std::cout << "2023/10/11\n";
+
+    std::cout << "player作成\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
